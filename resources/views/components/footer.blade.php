26e4be0ce9425b183bbe576bb5a4dee694c7302f
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2025-08-22 17:36:56 +0700

    refactor: Simplify footer layout by removing unused sections and optimizing contact information display

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -10,118 +10,21 @@
                   Menjaga ketertiban, keamanan, dan ketenteraman masyarakat Kota Tasikmalaya dengan profesional dan berkomitmen.
               </p>
           </div>
-          <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 lg:w-2/3">
+          <div class="grid grid-cols-1 gap-6 lg:w-2/3">
               <div>
                   <h2 class="mb-3 sm:mb-4 text-sm font-semibold text-white uppercase">Layanan</h2>
-                  <ul class="text-gray-200 font-medium text-xs sm:text-sm">
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Patroli Keamanan</a>
-                      </li>
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Pengawasan Ketertiban</a>
-                      </li>
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Penegakan Perda</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline hover:text-yellow-300">Bantuan Darurat</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="mb-3 sm:mb-4 text-sm font-semibold text-white uppercase">Informasi</h2>
-                  <ul class="text-gray-200 font-medium text-xs sm:text-sm">
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Berita Terkini</a>
-                      </li>
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Pengumuman</a>
-                      </li>
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Galeri Kegiatan</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline hover:text-yellow-300">Kontak</a>
-                      </li>
-                  </ul>
-              </div>
-              <div class="sm:col-span-2 lg:col-span-1">
-                  <h2 class="mb-3 sm:mb-4 text-sm font-semibold text-white uppercase">Kontak Kami</h2>
-                  <ul class="text-gray-200 font-medium text-xs sm:text-sm space-y-2 sm:space-y-3">
-                      <li class="flex items-start">
-                          <span class="text-yellow-300 mr-2 mt-0.5 text-sm">📍</span> 
-                          <span class="leading-relaxed">Jl. Sutisna Senjaya No.1, Tasikmalaya</span>
-                      </li>
-                      <li class="flex items-center">
-                          <span class="text-yellow-300 mr-2 text-sm">📞</span> 
-                          <a href="tel:0265123456" class="hover:text-yellow-300">(0265) 123456</a>
-                      </li>
-                      <li class="flex items-start">
-                          <span class="text-yellow-300 mr-2 mt-0.5 text-sm">✉️</span> 
-                          <a href="mailto:info@satpolpp-tasik.go.id" class="hover:text-yellow-300 break-all">info@satpolpp-tasik.go.id</a>
-                      </li>
-                      <li class="flex items-center">
-                          <span class="text-yellow-300 mr-2 text-sm">🕒</span> 
-                          <span>24 Jam Siaga</span>
-                      </li>
-                  </ul>
-              </div>
-              <div class="sm:col-span-2 lg:col-span-1">
-                  <h2 class="mb-3 sm:mb-4 text-sm font-semibold text-white uppercase">Legal</h2>
-                  <ul class="text-gray-200 font-medium text-xs sm:text-sm">
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Kebijakan Privasi</a>
-                      </li>
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Syarat & Ketentuan</a>
-                      </li>
-                      <li class="mb-2 sm:mb-3">
-                          <a href="#" class="hover:underline hover:text-yellow-300">Disclaimer</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline hover:text-yellow-300">Sitemap</a>
-                      </li>
-                  </ul>
+                  <div class="mt-4 text-gray-200 text-xs sm:text-sm leading-relaxed">
+                      Jl. Sutisna Senjaya No.1, Tasikmalaya<br>
+                      Telp: <a href="tel:0265123456" class="hover:text-yellow-300">(0265) 123456</a><br>
+                      Email: <a href="mailto:info@satpolpp-tasik.go.id" class="hover:text-yellow-300 break-all">info@satpolpp-tasik.go.id</a>
+                  </div>
               </div>
           </div>
       </div>
       <hr class="my-4 sm:my-6 border-gray-200 sm:mx-auto lg:my-8" />
-      <div class="flex flex-col space-y-3 sm:flex-row sm:items-center sm:justify-between sm:space-y-0">
-          <span class="text-xs sm:text-sm text-gray-200 text-center sm:text-left">© 2025 <a href="/" class="hover:underline hover:text-yellow-300">Satpol PP Kota Tasikmalaya</a>. Semua hak dilindungi.
+      <div>
+          <span class="text-xs sm:text-sm text-gray-200 text-center block">© 2025 <a href="/" class="hover:underline hover:text-yellow-300">Satpol PP Kota Tasikmalaya</a>. Semua hak dilindungi.
           </span>
-          <div class="flex justify-center space-x-4 sm:space-x-5 sm:justify-end">
-              <a href="#" class="text-gray-200 hover:text-yellow-300" title="Facebook">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                        <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
-                    </svg>
-                  <span class="sr-only">Facebook page</span>
-              </a>
-              <a href="#" class="text-gray-200 hover:text-yellow-300" title="Instagram">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
-                    </svg>
-                  <span class="sr-only">Instagram page</span>
-              </a>
-              <a href="#" class="text-gray-200 hover:text-yellow-300" title="Twitter">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
-                    <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd"/>
-                </svg>
-                  <span class="sr-only">Twitter page</span>
-              </a>
-              <a href="#" class="text-gray-200 hover:text-yellow-300" title="YouTube">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
-                        <path fill-rule="evenodd" d="M19.7 3.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839A4.225 4.225 0 0 0 .3 3.037a30.148 30.148 0 0 0-.2 3.464v1.8a30.12 30.12 0 0 0 .2 3.464c.095.726.408 1.37.789 1.964.5.506 1.188.808 1.984.839 2.771.2 6.928.2 6.928.2s4.159 0 6.926-.2a2.84 2.84 0 0 0 1.984-.839 4.225 4.225 0 0 0 .789-1.964 30.12 30.12 0 0 0 .2-3.464v-1.8a30.148 30.148 0 0 0-.2-3.464ZM8.001 8.8V4.6l4.757 2.1L8.001 8.8Z" clip-rule="evenodd"/>
-                    </svg>
-                  <span class="sr-only">YouTube channel</span>
-              </a>
-              <a href="#" class="text-gray-200 hover:text-yellow-300" title="Email">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                    <path d="M10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A2 2 0 0 0 .641.541l9.395 7.737Z"/>
-                    <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
-                </svg>
-                  <span class="sr-only">Email contact</span>
-              </a>
-          </div>
       </div>
     </div>
 </footer>
